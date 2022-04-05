3de58dd59c02b72684ed5e12b43d5fc843834d3f
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-04-04 22:06:47 -0400

    error out if the user hasn't installed Vulkan SDK
    it's not redneck if it works
    
    
    I've literally never used anything besides bash, so I don't *think* this has any bash-isms, but feel free to correct me if I'm wrong

--- Engine/Shaders/GLSL/compile.sh
+++ Engine/Shaders/GLSL/compile.sh
@@ -1,19 +1,26 @@
 mkdir -p bin 
 cd ./src
 
-for file in *.vert *.frag
-do
+if [[ "${VULKAN_SDK}" == "" ]] 
+then
+    echo "ERROR - Vulkan SDK not detected"
+    exit 1;
 
-    echo $VULKAN_SDK/bin/glslc $file -o ../bin/$file
-    val= $VULKAN_SDK/bin/glslc $file -o ../bin/$file
-    
-    if [ val != 0 ]
-    then
-    	echo Compile Successful
-    else
-    	read -n1 -r -p "Press any key to continue..."
-    fi
-    
-done
+else
+    for file in *.vert *.frag
+    do
+
+        echo $VULKAN_SDK/bin/glslc $file -o ../bin/$file
+        val= $VULKAN_SDK/bin/glslc $file -o ../bin/$file
+
+        if [ val != 0 ]
+        then
+            echo Compile Successful
+        else
+            read -n1 -r -p "Press any key to continue..."
+        fi
+
+    done
+fi
 
 cd ..
