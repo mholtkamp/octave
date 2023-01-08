6f3bd2721c7c46f9771aeea6d1da625bf0115d8d
Martin Holtkamp <mholt012@gmail.com>
2023-01-08 10:37:18 -0500

    Add context action for instantiating widget maps

--- Engine/Include/Editor/EditorUtils.h
+++ Engine/Include/Editor/EditorUtils.h
@@ -8,6 +8,7 @@
 #include "EngineTypes.h"
 struct AssetStub;
 class AssetDir;
+class WidgetMap;
 
 void EditorShowCursor(bool show);
 void EditorCenterCursor();
@@ -16,3 +17,4 @@ void EditorGetWindowCenter(int32_t& x, int32_t& y);
 glm::vec3 EditorGetFocusPosition();
 AssetStub* EditorAddUniqueAsset(const char* baseName, AssetDir* dir, TypeId assetType, bool autoCreate);
 std::string EditorGetAssetNameFromPath(const std::string& path);
+void EditorInstantiateMappedWidget(WidgetMap* widgetMap);
