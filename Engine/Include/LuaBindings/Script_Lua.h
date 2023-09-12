5ef29d258c07e6d750fcd6e96fc89f8987dd6b03
Martin Holtkamp <mholt012@gmail.com>
2023-09-12 14:51:23 -0400

    Add function for loading an entire script directory.

--- Engine/Include/LuaBindings/Script_Lua.h
+++ Engine/Include/LuaBindings/Script_Lua.h
@@ -12,6 +12,7 @@ struct Script_Lua
     static int Inherit(lua_State* L);
     static int New(lua_State* L);
     static int GarbageCollect(lua_State* L);
+    static int LoadDirectory(lua_State* L);
 
     static void Bind();
 };
