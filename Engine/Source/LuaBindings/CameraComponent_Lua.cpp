9175dcdf73aca0f3ac6f071bde8ecba1a91d834b
Martin Holtkamp <mholt012@gmail.com>
2023-09-24 11:41:12 -0400

    Add lua bindings for converting screen to world position.

--- Engine/Source/LuaBindings/CameraComponent_Lua.cpp
+++ Engine/Source/LuaBindings/CameraComponent_Lua.cpp
@@ -3,6 +3,8 @@
 #include "LuaBindings/Vector_Lua.h"
 #include "LuaBindings/LuaUtils.h"
 
+#include "Components/PrimitiveComponent.h"
+
 #if LUA_ENABLED
 
 int CameraComponent_Lua::SetPerspective(lua_State* L)
@@ -184,6 +186,34 @@ int CameraComponent_Lua::WorldToScreenPosition(lua_State* L)
     return 1;
 }
 
+int CameraComponent_Lua::ScreenToWorldPosition(lua_State* L)
+{
+    CameraComponent* comp = CHECK_CAMERA_COMPONENT(L, 1);
+    int32_t x = CHECK_INTEGER(L, 2);
+    int32_t y = CHECK_INTEGER(L, 3);
+
+    glm::vec3 ret = comp->ScreenToWorldPosition(x, y);
+
+    Vector_Lua::Create(L, ret);
+    return 1;
+}
+
+int CameraComponent_Lua::TraceScreenToWorld(lua_State* L)
+{
+    CameraComponent* comp = CHECK_CAMERA_COMPONENT(L, 1);
+    int32_t x = CHECK_INTEGER(L, 2);
+    int32_t y = CHECK_INTEGER(L, 3);
+    uint8_t colMask = 0xff;
+    if (!lua_isnone(L, 4)) { colMask = (uint8_t)CHECK_INTEGER(L, 4); }
+
+    PrimitiveComponent* hitComp = nullptr;
+    glm::vec3 worldPos = comp->TraceScreenToWorld(x, y, colMask, &hitComp);
+
+    Vector_Lua::Create(L, worldPos);
+    Component_Lua::Create(L, hitComp);
+    return 2;
+}
+
 void CameraComponent_Lua::Bind()
 {
     lua_State* L = GetLua();
@@ -244,6 +274,12 @@ void CameraComponent_Lua::Bind()
 
     lua_pushcfunction(L, WorldToScreenPosition);
     lua_setfield(L, mtIndex, "WorldToScreenPosition");
+
+    lua_pushcfunction(L, ScreenToWorldPosition);
+    lua_setfield(L, mtIndex, "ScreenToWorldPosition");
+
+    lua_pushcfunction(L, TraceScreenToWorld);
+    lua_setfield(L, mtIndex, "TraceScreenToWorld");
 
     lua_pop(L, 1);
     OCT_ASSERT(lua_gettop(L) == 0);
