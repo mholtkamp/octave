48c228261cb0ee7ceb055d1327438b49f56dccf5
Martin Holtkamp <mholt012@gmail.com>
2024-08-06 20:56:56 -0400

    Disable imgui gamepad navigation

--- Engine/Source/Editor/EditorImgui.cpp
+++ Engine/Source/Editor/EditorImgui.cpp
@@ -3030,8 +3030,7 @@ void EditorImguiInit()
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
