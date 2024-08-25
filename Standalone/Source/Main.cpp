92ee20a74fe4fc0357786d53bcb8ecf9265397c8
Martin Holtkamp <mholt012@gmail.com>
2024-08-25 13:14:51 -0400

    Remove old working directory hack. Doesn't seem to be necessary at all on Wii.

--- Standalone/Source/Main.cpp
+++ Standalone/Source/Main.cpp
@@ -62,31 +62,11 @@ InitOptions OctPreInitialize()
     initOptions.mVersion = 0;
 
 #if EMBEDDED_ENABLED
-    // Note to self:
-    // If you want to run without embedded assets on these consoles, then:
-    // (1) Set EMBED_ALL_ASSETS to 0 in Constants.h
-    // (2) Compile and launch the editor. Ctrl+B to build.
-    // (3) Move Rocket/Assets to sd:/Rocket/Assets
-    // (4) Move Engine/Assets to sd:/Engine/Assets
-    // (5) Move Rocket.octp and AssetRegistry.txt into the sd:/Rocket folder
-    // (6) Set the initOptions.mUseAssetRegistry to true in this file.
-    // (7) On Wii, you might want to set the working directory to the app's folder so 
-    //     that you don't clutter the root of the SD card. I forget if on 3DS your working
-    //     directory is where you launch the executable from, but on Wii it resets to sd:/
-    //     You can set the working directory in the initOptions.mWorkingDirectory
-    //     For example: initOptions.mWorkingDirectory = "sd:/apps/RetroLeagueGX";
-    // (8) Compile Rocket and place Rocket.dol (or other exe fmt) in normal place on sd card.
-    // (9) Launch the executable and hope it works!
     initOptions.mEmbeddedAssetCount = gNumEmbeddedAssets;
     initOptions.mEmbeddedAssets = gEmbeddedAssets;
     initOptions.mEmbeddedScriptCount = gNumEmbeddedScripts;
     initOptions.mEmbeddedScripts = gEmbeddedScripts;
 #endif
-
-#if PLATFORM_WII
-    initOptions.mWorkingDirectory = "sd://apps/Octave";
-#endif
-
 
     // Open Engine.ini and check for project name
     bool embedded = false;
