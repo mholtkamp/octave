23c113b0b218a8cdd3c0303bfa76d04c3bf2806c
mholtkamp <mholt012@gmail.com>
2025-09-06 22:31:15 -0400

    Remove OCT_SCENE_CONVERSION code

--- Engine/Source/Engine/Asset.h
+++ Engine/Source/Engine/Asset.h
@@ -49,10 +49,6 @@ struct AssetHeader
     uint32_t mVersion = ASSET_VERSION_CURRENT;
     TypeId mType = INVALID_TYPE_ID;
     uint8_t mEmbedded = false;
-
-#if OCT_SCENE_CONVERSION
-    TypeId mOldType = INVALID_TYPE_ID;
-#endif
 };
 
 struct AssetStub
@@ -147,8 +143,4 @@ protected:
 
     std::string mName = "Asset";
     int32_t mRefCount = 0;
-
-#if OCT_SCENE_CONVERSION
-    TypeId mOldType = INVALID_TYPE_ID;
-#endif
 };
