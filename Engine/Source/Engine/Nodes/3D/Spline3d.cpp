6a94c03aabf08cb163135628ed909e20fa33079a
mholtkamp <mholt012@gmail.com>
2026-02-13 21:09:05 -0500

    Fix some 3DS compilation errors

--- Engine/Source/Engine/Nodes/3D/Spline3d.cpp
+++ Engine/Source/Engine/Nodes/3D/Spline3d.cpp
@@ -68,7 +68,7 @@ void Spline3D::Create()
     Node3D::Create();
     SetName("Spline");
 
-    EnsureLinkSlots((uint32_t)glm::clamp(mGeneratedLinkCount, 1, 64));
+    EnsureLinkSlots((uint32_t)glm::clamp<int32_t>(mGeneratedLinkCount, 1, 64));
 
     // Create initial point
     Box3D* p1 = CreateChild<Box3D>("point1");
@@ -84,7 +84,7 @@ void Spline3D::Start()
     Node3D::Start();
     mTravel = 0.0f;
 
-    mGeneratedLinkCount = glm::clamp(mGeneratedLinkCount, 1, 64);
+    mGeneratedLinkCount = glm::clamp<int32_t>(mGeneratedLinkCount, 1, 64);
     EnsureLinkSlots((uint32_t)mGeneratedLinkCount);
 
     Node* root = GetWorld() ? GetWorld()->GetRootNode() : nullptr;
@@ -217,7 +217,7 @@ bool Spline3D::IsPaused() const
 
 void Spline3D::EnsureLinkSlots(uint32_t count)
 {
-    count = glm::clamp(count, 1u, 64u);
+    count = glm::clamp<uint32_t>(count, 1u, 64u);
     mLinks.resize(count);
     mGeneratedLinkCount = (int32_t)count;
 }
@@ -1094,7 +1094,7 @@ void Spline3D::LoadStream(Stream& stream, Platform platform, uint32_t version)
     if (stream.GetPos() < stream.GetSize())
     {
         uint32_t linkCount = stream.ReadUint32();
-        linkCount = glm::clamp(linkCount, 1u, 64u);
+        linkCount = glm::clamp<uint32_t>(linkCount, 1u, 64u);
         mLinks.clear();
         mLinks.resize(linkCount);
         mGeneratedLinkCount = (int32_t)linkCount;
@@ -1124,7 +1124,7 @@ void Spline3D::LoadStream(Stream& stream, Platform platform, uint32_t version)
     }
     else
     {
-        EnsureLinkSlots((uint32_t)glm::clamp(mGeneratedLinkCount, 1, 64));
+        EnsureLinkSlots((uint32_t)glm::clamp<int32_t>(mGeneratedLinkCount, 1, 64));
     }
 }
 
@@ -1132,7 +1132,7 @@ void Spline3D::GatherProperties(std::vector<Property>& props)
 {
     Node3D::GatherProperties(props);
 
-    mGeneratedLinkCount = glm::clamp(mGeneratedLinkCount, 1, 64);
+    mGeneratedLinkCount = glm::clamp<int32_t>(mGeneratedLinkCount, 1, 64);
     EnsureLinkSlots((uint32_t)mGeneratedLinkCount);
 
     {
@@ -1163,7 +1163,7 @@ void Spline3D::GatherProperties(std::vector<Property>& props)
 
     {
         SCOPED_CATEGORY("Spline Linking");
-        mGeneratedLinkCount = glm::clamp(mGeneratedLinkCount, 1, 64);
+        mGeneratedLinkCount = glm::clamp<int32_t>(mGeneratedLinkCount, 1, 64);
         EnsureLinkSlots((uint32_t)mGeneratedLinkCount);
 
         props.push_back(Property(DatumType::Bool, "Generate Link", this, &sGenerateLink11));
